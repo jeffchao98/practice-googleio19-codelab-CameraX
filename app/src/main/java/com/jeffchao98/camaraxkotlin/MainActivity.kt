e734961c311380a0901edae5c01969e6829771af
jeffchao98 <philadelphiafs@gmail.com>
2019-10-31 00:47:20 +0800

    FEATURE #2 - Start a new project - Init the new Android project in Kotlin

---
+++ app/src/main/java/com/jeffchao98/camaraxkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jeffchao98.camaraxkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
